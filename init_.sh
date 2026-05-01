aa197a159d9c7efe0d0d47bd5dfdea673fcf79c6
oo33 <hasnaouiyacine59@gmail.com>
2026-05-01 02:23:21 +0100

    fix_bulk1

--- init_.sh
+++ init_.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -43,6 +43,8 @@ for i in $(seq 0 3); do
   echo "tor-$SOCKS ready"
 done
 RAND=$(cat /dev/urandom | tr -dc 'a-z' | head -c2)$(cat /dev/urandom | tr -dc '0-9' | head -c2)
+RAND1=$(echo "$RAND" | cut -c1-2)
+RAND2=$(echo "$RAND" | cut -c3-4)
 
 # Start thor-session containers
 echo "Starting thor-session containers..."
@@ -51,7 +53,7 @@ for i in $(seq 0 3); do
   API=$((5000 + i))
   SESSION=$((i + 1))
   HOST_PORT=$((8080 + i))
-  docker run -d --restart=on-failure --name thor-session-$SESSION --hostname ${RAND:0:2}-${RAND:2:2}-sec-$HOSTNAME-sec-$SESSION --network tor-net \
+  docker run -d --restart=on-failure --name thor-session-$SESSION --hostname ${RAND1}-${RAND2}-sec-$HOSTNAME-sec-$SESSION --network tor-net \
     -e SOCKS_PORT=$SOCKS -e API_PORT=$API -e TOR_HOST=tor-$SOCKS \
     -p $HOST_PORT:8080 thor-session:v1.44
 done
